8cb8727f7cfe35ad4bbce59414756ac96dcd7bb5
uyhunghang <lytrieuuy03@gmail.com>
2024-11-28 17:03:10 +0700

    mac ia

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,27 +1,30 @@
 import React from "react";
 import Header from "./header";
 import logo from './component/BachKhoaLogo.png';
+import user from './component/user.png';
+import book from './component/Book.png';
+import printer from './component/printer.png';
+import payment from './component/Icon.png';
 import background from './component/bgbk.png';
 import dbstyles from "./dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <div className="container">
+    <div className={dbstyles.container}>
     <div className={dbstyles.dashboard}>
       <Header logo={logo}/>
 
       <main className={dbstyles.main}>
+      <div className={dbstyles.welcome_row}>
         <div className={dbstyles.welcome_section}>
           <h1 className={dbstyles.greeting}>Good Morning, Thao!</h1>
           <p className={dbstyles.greeting_subtext}>Ready to print something?</p>
           <p className={dbstyles.description}>
             We provide the printing service for all students and lecturers in HCMUT campus.
           </p>
-        </div>
-
-        <div className={dbstyles.actions}>
+          <div className={dbstyles.actions}>
           <div className={dbstyles.action_item}>
-            <i className={dbstyles.action_icon}>👤</i>
+            <img src={user} alt="User" className={dbstyles.action_icon}></img>
             <div>
               <p className={dbstyles.action_title}>STUDENT INFORMATION</p>
               <p className={dbstyles.action_description}>View your information</p>
@@ -30,7 +33,7 @@ const Dashboard = () => {
           </div>
 
           <div className={dbstyles.action_item}>
-            <i className={dbstyles.action_icon}>🖨️</i>
+          <img src={printer} alt="Printer" className={dbstyles.action_icon}></img>
             <div>
               <p className={dbstyles.action_title}>PRINT DOCUMENT</p>
               <p className={dbstyles.action_description}>Print your document</p>
@@ -39,7 +42,7 @@ const Dashboard = () => {
           </div>
 
           <div className={dbstyles.action_item}>
-            <i className={dbstyles.action_icon}>📜</i>
+          <img src={book} alt="Book" className={dbstyles.action_icon}></img>
             <div>
               <p className={dbstyles.action_title}>VIEW HISTORY</p>
               <p className={dbstyles.action_description}>View history of your printing document</p>
@@ -48,7 +51,7 @@ const Dashboard = () => {
           </div>
 
           <div className={dbstyles.action_item}>
-            <i className={dbstyles.action_icon}>🏦</i>
+          <img src={payment} alt="Payment" className={dbstyles.action_icon}></img>
             <div>
               <p className={dbstyles.action_title}>PAYMENT</p>
               <p className={dbstyles.action_description}>Buy more paper to print your documents!</p>
@@ -56,9 +59,12 @@ const Dashboard = () => {
             <span className={dbstyles.action_arrow}>→</span>
           </div>
         </div>
+        </div>
         <div className={dbstyles.image_section}>
             <img src={background} alt=" Background Bach Khoa" className={dbstyles.right_image} />
-          </div>
+        </div>
+        </div>
+        
       </main>
     </div>
     </div>
