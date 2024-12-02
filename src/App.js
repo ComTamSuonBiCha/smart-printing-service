e3102d6edba448b1fb73326b95e64626991b2bcc
Khoa <khoana@nexlesoft.com>
2024-12-02 10:12:55 +0700

    include the routing mechanic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import Login from './login'
-import Header from './header'
-import Dashboard from './dashboard'
-import Homelogin from './homelogin';
-import HomeloginHeader from './homeloginHeader'
-
-// import loginHeader from './loginHeader'
+import React, { Fragment } from "react";
+import { routes } from "./routes";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Homelogin/>;
+  return (
+    <Routes>
+      {routes.map((route, index) => {
+        const Layout = route.layout ? route.layout : Fragment;
+        const Page = route.component;
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Layout>
+                <Page />
+              </Layout>
+            }
+          />
+        );
+      })}
+    </Routes>
+  );
 }
 
 export default App;
