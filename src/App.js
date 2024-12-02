d6d1fe6b39581d9a6a81caf0d73f76aaf50028dd
uyhunghang <lytrieuuy03@gmail.com>
2024-12-02 14:55:23 +0700

    print-main-update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Fragment } from "react";
 import { routes } from "./routes";
 import { Route, Routes } from "react-router-dom";
+import DashboardStudent from "./dashboard";
+import PrintDocument from "./printdocument";
 
 function App() {
   return (
-    <Routes>
-      {routes.map((route, index) => {
-        const Layout = route.layout ? route.layout : Fragment;
-        const Page = route.component;
-        return (
-          <Route
-            key={index}
-            path={route.path}
-            element={
-              <Layout>
-                <Page />
-              </Layout>
-            }
-          />
-        );
-      })}
-    </Routes>
+    <PrintDocument/>
   );
 }
 
--- src/printdocument.js
+++ src/printdocument.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./printdocument.css";
+import docustyle from "./printdocument.module.css";
 import Header from "./header";
 import logo from './component/BachKhoaLogo.png';
 import upload from './component/File-upload.png';
@@ -8,47 +8,47 @@ import properties from './component/Settings-adjust.png';
 
 function PrintDocument() {
   return (
-    <div className="container">
-    <div className="print-document">
+    <div className={docustyle.container}>
+    <div className={docustyle.print_document}>
       <Header logo={logo}/>
-      <main className="main-content">
-        <div className="left-side">
+      <main className={docustyle.main_content}>
+        <div className={docustyle.left_side}>
         <h1>Print Document</h1>
         <p>Please fill out 3 components below before proceeding to the next step!</p>
-        <div className="action-list">
-            <div className="action-item">
-                <div className="display-item">
-                    <img src={upload} alt="File-Upload" className="icon"></img>
-                    <p className="icon-title">UPLOAD FILE</p>
+        <div className={docustyle.action_list}>
+            <div className={docustyle.action_item}>
+                <div className={docustyle.display_item}>
+                    <img src={upload} alt="File-Upload" className={docustyle.icon}></img>
+                    <p className={docustyle.icon_title}>UPLOAD FILE</p>
                 </div>
-                <p className="action-desciption">Upload your document here (PDF, DOCX, XLS, XLSX)</p>
-                <div className="display-item">
-                    <p className="action-desciption">NAME</p> <div className="info-box"></div>
+                <p className={docustyle.action_desciption}>Upload your document here (PDF, DOCX, XLS, XLSX)</p>
+                <div className={docustyle.display_item}>
+                    <p className={docustyle.action_desciption}>NAME</p> <div className="info-box"></div>
                 </div>
 
             </div>
-            <div className="action-item">
-                <div className="display-item">
-                    <img src={properties} alt="Properties" className="icon"></img>
-                    <p className="icon-title">SET UP YOUR PROPERTIES</p>
+            <div className={docustyle.action_item}>
+                <div className={docustyle.display_item}>
+                    <img src={properties} alt="Properties" className={docustyle.icon}></img>
+                    <p className={docustyle.icon_title}>SET UP YOUR PROPERTIES</p>
                 </div>
-                <p className="action-desciption">Set up your printing properties</p>
+                <p className={docustyle.action_desciption}>Set up your printing properties</p>
 
             </div>
-            <div className="action-item">
-                <div className="display-item">
-                    <img src={printer} alt="Printer" className="icon"></img>
-                    <p className="icon-title">CHOOSE PRINTER</p>
+            <div className={docustyle.action_item}>
+                <div className={docustyle.display_item}>
+                    <img src={printer} alt="Printer" className={docustyle.icon}></img>
+                    <p className={docustyle.icon_title}>CHOOSE PRINTER</p>
                 </div>
-                <p className="action-desciption">Choose available printer</p>
+                <p className={docustyle.action_desciption}>Choose available printer</p>
 
             </div>
         </div>
-        <button className="print-btn">
+        <button className={docustyle.print_btn}>
           <b>PRINT</b>
         </button>
         </div>
-        <div className="right-side">
+        <div className={docustyle.right_side}>
             <p>Hi guys</p>
         </div>
       </main>
