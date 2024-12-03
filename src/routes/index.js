32addac0210d00a8598e85d6ef078562fa6df1c0
uyhunghang <lytrieuuy03@gmail.com>
2024-12-04 00:52:53 +0700

    upload-file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,10 @@ import LoginPage from "../homelogin";
 import Login from "../login";
 import SPSODashboard from "../pages/spso/SPSODashboard";
 import Stats from "../pages/stats";
+import PrintDocument from "../printdocument";
 
 const routes = [
-  { path: "/", component: Dashboard },
+  { path: "/", component: PrintDocument},
   { path: "/log-in", component: LoginPage },
   { path: "/log", component: Login },
   { path: "/spso", component: SPSODashboard },
