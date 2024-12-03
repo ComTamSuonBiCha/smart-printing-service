935ae74ee56cf04ad05254a5dd1130e4423b402d
Khoa <khoana@nexlesoft.com>
2024-12-03 07:06:53 +0700

    fix routing bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import HomeLogin from "./homelogin" 
-import Home from "./home"
-import Login from "./login" 
 import React, { Fragment } from "react";
 import { routes } from "./routes";
 import { Route, Routes } from "react-router-dom";
-import DashboardStudent from "./dashboard";
-import PrintDocument from "./printdocument";
 
 function App() {
   return (
@@ -14,21 +9,15 @@ function App() {
         const Layout = route.layout ? route.layout : Fragment;
         const Page = route.component;
         return (
-          <Route>
-            <Route index element = {<Home/>} />
-            <Route path="/HomeLogin" element = {<HomeLogin/>}/>
-            <Route path="/Login" element = {<Login/>}/>
-            <Route path="/Dashboard" element = {<DashboardStudent/>}/>
-
-          </Route>
-            // key={index}
-            // path={route.path}
-            // element={
-            //   <Layout>
-            //     <Page />
-            //   </Layout>
-            //}
-          // />
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Layout>
+                <Page />
+              </Layout>
+            }
+          />
         );
       })}
     </Routes>
