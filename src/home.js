695c57c9d5883aa9055c43c046a113db0d4a477f
Thao Le <lpthao150504@gmail.com>
2024-12-07 11:48:27 +0700

    change route and home page

--- src/home.js
+++ src/home.js
@@ -2,11 +2,14 @@ import React from "react";
 import HomeloginHeader from './homeloginHeader';
 import sectionStyles from "./home.module.css";
 import logo from './component/BachKhoaLogo.png';
+import { useNavigate } from "react-router-dom";
 
 function HeroSection() {
+  const navigate = useNavigate();
+  const gotoLogin = () => {navigate("/loginpage")};
   return (
     <div className="container">
-      <HomeloginHeader logo = {logo}/>
+      {/* <HomeloginHeader logo = {logo}/> */}
       <div className={sectionStyles.hero_content}>
         <h1 className={sectionStyles.hero_title}>HCMUT Student Printing Service System</h1>
         <p className={sectionStyles.hero_subtitle}>A place to print your document</p>
